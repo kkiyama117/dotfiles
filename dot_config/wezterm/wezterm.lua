a5e97507fde622ecbb77aa9294e2f30667888e5c
kkiyama117 <k.kiyama117@gmail.com>
2024-07-21 19:51:51 +0900

    Update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -42,124 +42,37 @@ config = {
   -- KEYS ===================================================================
   -- https://wezfurlong.org/wezterm/config/keys.html
   leader = { key = 'a', mods = 'SUPER', timeout_milliseconds = 1000 },
-  disable_default_key_bindings = true,
+  --disable_default_key_bindings = true,
   keys = {
     { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  },
-  key_tables = {
-    copy_mode = {
-      { key = 'Tab', mods = 'NONE', action = act.CopyMode 'MoveForwardWord' },
-      { key = 'Tab', mods = 'SHIFT', action = act.CopyMode 'MoveBackwardWord' },
-      { key = 'Enter', mods = 'NONE', action = act.CopyMode 'MoveToStartOfNextLine' },
-      { key = 'Escape', mods = 'NONE', action = act.CopyMode 'Close' },
-      { key = 'Space', mods = 'NONE', action = act.CopyMode{ SetSelectionMode =  'Cell' } },
-      { key = '$', mods = 'NONE', action = act.CopyMode 'MoveToEndOfLineContent' },
-      { key = '$', mods = 'SHIFT', action = act.CopyMode 'MoveToEndOfLineContent' },
-      { key = ',', mods = 'NONE', action = act.CopyMode 'JumpReverse' },
-      { key = '0', mods = 'NONE', action = act.CopyMode 'MoveToStartOfLine' },
-      { key = ';', mods = 'NONE', action = act.CopyMode 'JumpAgain' },
-      { key = 'F', mods = 'NONE', action = act.CopyMode{ JumpBackward = { prev_char = false } } },
-      { key = 'F', mods = 'SHIFT', action = act.CopyMode{ JumpBackward = { prev_char = false } } },
-      { key = 'G', mods = 'NONE', action = act.CopyMode 'MoveToScrollbackBottom' },
-      { key = 'G', mods = 'SHIFT', action = act.CopyMode 'MoveToScrollbackBottom' },
-      { key = 'H', mods = 'NONE', action = act.CopyMode 'MoveToViewportTop' },
-      { key = 'H', mods = 'SHIFT', action = act.CopyMode 'MoveToViewportTop' },
-      { key = 'L', mods = 'NONE', action = act.CopyMode 'MoveToViewportBottom' },
-      { key = 'L', mods = 'SHIFT', action = act.CopyMode 'MoveToViewportBottom' },
-      { key = 'M', mods = 'NONE', action = act.CopyMode 'MoveToViewportMiddle' },
-      { key = 'M', mods = 'SHIFT', action = act.CopyMode 'MoveToViewportMiddle' },
-      { key = 'O', mods = 'NONE', action = act.CopyMode 'MoveToSelectionOtherEndHoriz' },
-      { key = 'O', mods = 'SHIFT', action = act.CopyMode 'MoveToSelectionOtherEndHoriz' },
-      { key = 'T', mods = 'NONE', action = act.CopyMode{ JumpBackward = { prev_char = true } } },
-      { key = 'T', mods = 'SHIFT', action = act.CopyMode{ JumpBackward = { prev_char = true } } },
-      { key = 'V', mods = 'NONE', action = act.CopyMode{ SetSelectionMode =  'Line' } },
-      { key = 'V', mods = 'SHIFT', action = act.CopyMode{ SetSelectionMode =  'Line' } },
-      { key = '^', mods = 'NONE', action = act.CopyMode 'MoveToStartOfLineContent' },
-      { key = '^', mods = 'SHIFT', action = act.CopyMode 'MoveToStartOfLineContent' },
-      { key = 'b', mods = 'NONE', action = act.CopyMode 'MoveBackwardWord' },
-      { key = 'b', mods = 'ALT', action = act.CopyMode 'MoveBackwardWord' },
-      { key = 'b', mods = 'CTRL', action = act.CopyMode 'PageUp' },
-      { key = 'c', mods = 'CTRL', action = act.CopyMode 'Close' },
-      { key = 'd', mods = 'CTRL', action = act.CopyMode{ MoveByPage = (0.5) } },
-      { key = 'e', mods = 'NONE', action = act.CopyMode 'MoveForwardWordEnd' },
-      { key = 'f', mods = 'NONE', action = act.CopyMode{ JumpForward = { prev_char = false } } },
-      { key = 'f', mods = 'ALT', action = act.CopyMode 'MoveForwardWord' },
-      { key = 'f', mods = 'CTRL', action = act.CopyMode 'PageDown' },
-      { key = 'g', mods = 'NONE', action = act.CopyMode 'MoveToScrollbackTop' },
-      { key = 'g', mods = 'CTRL', action = act.CopyMode 'Close' },
-      { key = 'h', mods = 'NONE', action = act.CopyMode 'MoveLeft' },
-      { key = 'j', mods = 'NONE', action = act.CopyMode 'MoveDown' },
-      { key = 'k', mods = 'NONE', action = act.CopyMode 'MoveUp' },
-      { key = 'l', mods = 'NONE', action = act.CopyMode 'MoveRight' },
-      { key = 'm', mods = 'ALT', action = act.CopyMode 'MoveToStartOfLineContent' },
-      { key = 'o', mods = 'NONE', action = act.CopyMode 'MoveToSelectionOtherEnd' },
-      { key = 'q', mods = 'NONE', action = act.CopyMode 'Close' },
-      { key = 't', mods = 'NONE', action = act.CopyMode{ JumpForward = { prev_char = true } } },
-      { key = 'u', mods = 'CTRL', action = act.CopyMode{ MoveByPage = (-0.5) } },
-      { key = 'v', mods = 'NONE', action = act.CopyMode{ SetSelectionMode =  'Cell' } },
-      { key = 'v', mods = 'CTRL', action = act.CopyMode{ SetSelectionMode =  'Block' } },
-      { key = 'w', mods = 'NONE', action = act.CopyMode 'MoveForwardWord' },
-      { key = 'y', mods = 'NONE', action = act.Multiple{ { CopyTo =  'ClipboardAndPrimarySelection' }, { CopyMode =  'Close' } } },
-      { key = 'PageUp', mods = 'NONE', action = act.CopyMode 'PageUp' },
-      { key = 'PageDown', mods = 'NONE', action = act.CopyMode 'PageDown' },
-      { key = 'End', mods = 'NONE', action = act.CopyMode 'MoveToEndOfLineContent' },
-      { key = 'Home', mods = 'NONE', action = act.CopyMode 'MoveToStartOfLine' },
-      { key = 'LeftArrow', mods = 'NONE', action = act.CopyMode 'MoveLeft' },
-      { key = 'LeftArrow', mods = 'ALT', action = act.CopyMode 'MoveBackwardWord' },
-      { key = 'RightArrow', mods = 'NONE', action = act.CopyMode 'MoveRight' },
-      { key = 'RightArrow', mods = 'ALT', action = act.CopyMode 'MoveForwardWord' },
-      { key = 'UpArrow', mods = 'NONE', action = act.CopyMode 'MoveUp' },
-      { key = 'DownArrow', mods = 'NONE', action = act.CopyMode 'MoveDown' },
-    },
-    search_mode = {
-      { key = 'Enter', mods = 'NONE', action = act.CopyMode 'PriorMatch' },
-      { key = 'Escape', mods = 'NONE', action = act.CopyMode 'Close' },
-      { key = 'n', mods = 'CTRL', action = act.CopyMode 'NextMatch' },
-      { key = 'p', mods = 'CTRL', action = act.CopyMode 'PriorMatch' },
-      { key = 'r', mods = 'CTRL', action = act.CopyMode 'CycleMatchType' },
-      { key = 'u', mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
-      { key = 'PageUp', mods = 'NONE', action = act.CopyMode 'PriorMatchPage' },
-      { key = 'PageDown', mods = 'NONE', action = act.CopyMode 'NextMatchPage' },
-      { key = 'UpArrow', mods = 'NONE', action = act.CopyMode 'PriorMatch' },
-      { key = 'DownArrow', mods = 'NONE', action = act.CopyMode 'NextMatch' },
-    },
   },
 
   -- MOUSE ==================================================================
   -- https://wezfurlong.org/wezterm/config/mouse.html
-  disable_default_mouse_bindings = true,
+  --disable_default_mouse_bindings = true,
   mouse_bindings = {
     -- Change the default click behavior so that it only selects
     -- text and doesn't open hyperlinks
     {
       event = { Up = { streak = 1, button = 'Left' } },
       mods = 'NONE',
-      -- action = act.CompleteSelection 'ClipboardAndPrimarySelection',
-		  action = wezterm.action_callback(function(window, pane)
-			  local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-			  if has_selection then
-				  window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-				  window:perform_action(act.ClearSelection, pane)
-			  else
-				  window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-			  end
-		  end),
+      action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+		  --action = wezterm.action_callback(function(window, pane)
+			--  local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			--  if has_selection then
+			--	  window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+			--	  window:perform_action(act.ClearSelection, pane)
+			--  else
+			--	  window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			--  end
+		  --end),
     },
-
     -- and make CTRL-Click open hyperlinks
     {
       event = { Up = { streak = 1, button = 'Left' } },
       mods = 'CTRL',
       action = act.OpenLinkAtMouseCursor,
     },
-    -- mouse wheel scroll
-    {
-      event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-      mods = 'NONE',
-      action = act.ScrollByCurrentEventWheelDelta,
-    },
-    -- NOTE that binding only the 'Up' event can give unexpected behaviors.
-    -- Read more below on the gotcha of binding an 'Up' event only.
   },
   -- default_cursor_style = 'BlinkingBlock',
 
