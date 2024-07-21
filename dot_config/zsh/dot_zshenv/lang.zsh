c8a649bd2dc8886b49b6a67a2f9a4e291d0d0f7a
kkiyama117 <k.kiyama117@gmail.com>
2024-07-21 19:58:33 +0900

    Update

--- dot_config/zsh/dot_zshenv/lang.zsh
+++ dot_config/zsh/dot_zshenv/lang.zsh
@@ -1,19 +1,19 @@
 # mise path
 path=($XDG_DATA_HOME/mise/shims(N-/) $path)
+export MISE_DATA_DIR="${MISE_DATA_DIR:-$XDG_DATA_HOME/mise}"
 
 # Need to keep variables about Rust
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export RUSTUP_HOME="${RUSTUP_HOME:-$XDG_DATA_HOME/rustup}"
+export CARGO_HOME="${CARGO_HOME:-$XDG_DATA_HOME/cargo}"
 path=($CARGO_HOME/bin(N-/) $path)
 
 # I am a firm believer in only using python installed with rye.
-export RYE_HOME="$XDG_DATA_HOME/rye"
-export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/python_startup.py
+export RYE_HOME="${RYE_HOME:-$XDG_DATA_HOME/rye}"
+export PYTHONSTARTUP="${PYTHONSTARTUP:-"$XDG_CONFIG_HOME"/python/python_startup.py}"
 source $RYE_HOME/env
 
 # opam
 [[ ! -r "$XDG_DATA_HOME/opam-init/init.zsh" ]] || source "$XDG_DATA_HOME/opam-init/init.zsh"  > /dev/null 2> /dev/null
 
 # npm
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
-
+export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-"$XDG_CONFIG_HOME"/npm/npmrc}"
