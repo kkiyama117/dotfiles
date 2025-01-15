735dbdd7164c3287f7fbc559b6be0446ebff926a
kkiyama117 <k.kiyama117@gmail.com>
2025-01-15 22:18:37 +0900

    Fix error

---
+++ dot_local/share/chezmoi/dot_password_manager.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# https://www.chezmoi.io/user-guide/advanced/install-your-password-manager-on-init/
+# exit immediately if password-manager-binary is already in $PATH
+type password-manager-binary >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    # commands to install password-manager-binary on Darwin
+    ;;
+Linux)
+    # commands to install password-manager-binary on Linux
+    #if (( $+commands[bw] )); then
+    if [ "$(which bw)" == "" ]; then
+      sudo pacman -S bitwarden-cli --noconfirm --needed 
+    fi
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
