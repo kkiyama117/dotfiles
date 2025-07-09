5473107e073648fc4ca3831bd6a1dd25f3e587e9
kkiyama117 <k.kiyama117@gmail.com>
2025-07-09 12:00:52 +0900

    Update tmux config

--- dot_config/zsh/rc/my_plugins/tmux.zsh
+++ dot_config/zsh/rc/my_plugins/tmux.zsh
@@ -1,13 +1,16 @@
 tmux() {
   if [ "$#" -eq 0 ]; then
+    echo "Custom tmux" 
     # 引数なし -> main セッションへアタッチ / なければ作成
     command tmux attach-session -t main 2>/dev/null \
       || command tmux new-session -s main
     command tmux source-file ~/.config/tmux/tmux.conf
+    # Startup configuration for tmux
+    command tmux splitw -h -p 50 -t 0 'pwd'
+    command tmux selectw -t 0
   else
     # 引数あり→通常の tmux コマンドとして実行
     command tmux "$@"
-    command tmux source-file ~/.config/tmux/tmux.conf
   fi
 }
 
